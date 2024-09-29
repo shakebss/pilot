ArduPilot/MissionPlanner

CameraProtocol: add image lat/lon calculator
Also draw camera bounds on FlightData map

ExtLibs/ArduPilot/Mavlink/CameraProtocol.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Threading.Tasks;
 using Core.Geometry;
 using GeoAPI.DataStructures;
 using log4net;
+using MissionPlanner.Utilities;
 
 namespace MissionPlanner.ArduPilot.Mavlink
 {
@@ -22,9 +24,12 @@ namespace MissionPlanner.ArduPilot.Mavlink
         // Tracks whether we have received a `CAMERA_INFORMATION` message yet
         private bool have_camera_information = false;
 
-        public MAVLink.mavlink_camera_information_t CameraInformation { get; set; }
-        public MAVLink.mavlink_camera_settings_t CameraSettings { get; set; }
-        public MAVLink.mavlink_camera_capture_status_t CameraCaptureStatus { get; set; }
+        public MAVLink.mavlink_camera_information_t CameraInformation { get; private set; }
+        public MAVLink.mavlink_camera_settings_t CameraSettings { get; private set; }
+        public MAVLink.mavlink_camera_capture_status_t CameraCaptureStatus { get; private set; }
+        public MAVLink.mavlink_camera_fov_status_t CameraFOVStatus { get; private set; }
+
+        public static Dictionary<(byte, byte, byte), MAVLink.mavlink_video_stream_information_t> VideoStreams { get; private set; } = new Dictionary<(byte, byte, byte), MAVLink.mavlink_video_stream_information_t>();
 
         /// <summary>
         /// True if the camera has different modes, like image mode and video mode
@@ -161,13 +166,16 @@ namespace MissionPlanner.ArduPilot.Mavlink
             {
             case MAVLink.MAVLINK_MSG_ID.CAMERA_INFORMATION:
                 have_camera_information = true;
-                CameraInformation = ((MAVLink.mavlink_camera_information_t)message.data);
+                CameraInformation = (MAVLink.mavlink_camera_information_t)message.data;
                 break;
             case MAVLink.MAVLINK_MSG_ID.CAMERA_SETTINGS:
-                CameraSettings = ((MAVLink.mavlink_camera_settings_t)message.data);
+                CameraSettings = (MAVLink.mavlink_camera_settings_t)message.data;
                 break;
             case MAVLink.MAVLINK_MSG_ID.CAMERA_CAPTURE_STATUS:
-                CameraCaptureStatus = ((MAVLink.mavlink_camera_capture_status_t)message.data);
+                CameraCaptureStatus = (MAVLink.mavlink_camera_capture_status_t)message.data;
+                break;
+            case MAVLink.MAVLINK_MSG_ID.CAMERA_FOV_STATUS:
+                CameraFOVStatus = (MAVLink.mavlink_camera_fov_status_t)message.data;
                 break;
             }
         }
@@ -191,6 +199,19 @@ namespace MissionPlanner.ArduPilot.Mavlink
                 Task.Run(RequestCameraInformationAsync);
             }
 
+            // Request FOV status
+            Task.Run(async () =>
+            {
+                await parent.parent.doCommandAsync(
+                    parent.sysid, parent.compid,
+                    MAVLink.MAV_CMD.SET_MESSAGE_INTERVAL,
+                    (float)MAVLink.MAVLINK_MSG_ID.CAMERA_FOV_STATUS,
+                    interval_us,
+                    0, 0, 0, 0, 0
+                ).ConfigureAwait(false);
+            });
+
+            // Get camera settings
             if (HasModes || HasZoom || HasFocus)
             {
                 Task.Run(async () =>
@@ -323,6 +344,38 @@ namespace MissionPlanner.ArduPilot.Mavlink
                 zoom_level,
                 0, 0, 0, 0, 0
             );
+        }
+
+        public PointLatLngAlt CalculateImagePointLocation(double x, double y)
+        {
+            var imagePosition = new PointLatLngAlt(CameraFOVStatus.lat_image * 1e-7, CameraFOVStatus.lon_image * 1e-7, CameraFOVStatus.alt_image * 1e-3);
+            if(x == 0 && y == 0)
+            {
+                return imagePosition;
+            }
+
+            var camPosition = new PointLatLngAlt(CameraFOVStatus.lat_camera * 1e-7, CameraFOVStatus.lon_camera * 1e-7, CameraFOVStatus.alt_camera * 1e-3);
+
+            var height = camPosition.Alt - imagePosition.Alt;
+            if(height < 0)
+            {
+                return null;
+            }
+
+            var dist = camPosition.GetDistance(imagePosition);
+            var down_elevation = Math.Atan2(height, dist); // zero means pointing level, pi/2 is straight down
+            down_elevation += y / 2 * CameraFOVStatus.vfov * Math.PI / 180;
+            down_elevation = Math.Max(0.0001, down_elevation);
+            var out_distance = height * Math.Cos(down_elevation) / Math.Sin(down_elevation);
+            out_distance = Math.Min(out_distance, 1e5);
+
+            var side_angle = x / 2 * CameraFOVStatus.hfov * Math.PI / 180;
+            var side_distance = Math.Sqrt(out_distance * out_distance + height * height) * Math.Tan(side_angle);
+
+            var bearing = camPosition.GetBearing(imagePosition);
+            var pos = camPosition.newpos(bearing, out_distance).newpos(bearing + 90, side_distance);
+            pos.Alt = imagePosition.Alt;
+            return pos;
         }
     }
 }

GCSViews/GimbalVideoControl.cs
@@ -15,6 +15,7 @@ using static MAVLink;
 using MissionPlanner.GCSViews;
 using System.Threading.Tasks;
 using MissionPlanner.ArduPilot.Mavlink;
+using GMap.NET.WindowsForms;
 
 namespace MissionPlanner
 {
@@ -71,6 +72,7 @@ namespace MissionPlanner
             }
         }
 
+        private GMapOverlay videoBounds;
         public GimbalVideoControl()
         {
             InitializeComponent();
@@ -81,6 +83,9 @@ namespace MissionPlanner
 
             // Register the global key handler
             Application.AddMessageFilter(this);
+
+            videoBounds = new GMapOverlay("VideoBounds");
+            MainV2.instance.FlightData.gMapControl1.Overlays.Add(videoBounds);
         }
 
         private void loadPreferences()
@@ -376,6 +381,62 @@ namespace MissionPlanner
                 var loc = MainV2.comPort?.MAV?.cs.HomeLocation;
                 selectedGimbalManager?.SetROILocationAsync(loc.Lat, loc.Lng, loc.Alt, frame: MAV_FRAME.GLOBAL);
             }
+        }
+
+        private DateTime lastMouseMove = DateTime.MinValue;
+        private void VideoBox_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if(DateTime.Now > lastMouseMove.AddMilliseconds(100))
+            {
+                lastMouseMove = DateTime.Now;
+                videoBounds.Clear();
+
+                if (VideoBox.Image == null)
+                {
+                    return;
+                }
+
+                // Find the point within the image inside VideoBox, not just the VideoBox
+                var x = e.X;
+                var y = e.Y;
+                var imageWidth = Math.Min(VideoBox.Width, VideoBox.Height * VideoBox.Image.Width / VideoBox.Image.Height);
+                var imageHeight = Math.Min(VideoBox.Height, VideoBox.Width * VideoBox.Image.Height / VideoBox.Image.Width);
+                if (imageWidth < VideoBox.Width)
+                {
+                    x -= (VideoBox.Width - imageWidth) / 2;
+                    x *= VideoBox.Width / imageWidth;
+                    if (x < 0 || x >= imageWidth)
+                    {
+                        return;
+                    }
+                }
+                if (imageHeight < VideoBox.Height)
+                {
+                    y -= (VideoBox.Height - imageHeight) / 2;
+                    y *= VideoBox.Height / imageHeight;
+                    if (y < 0 || y >= imageHeight)
+                    {
+                        return;
+                    }
+                }
+
+
+                var point = selectedCamera?.CalculateImagePointLocation(2 * x / (float)imageWidth - 1, 2 * y / (float)imageHeight - 1);
+                if (point != null)
+                {
+                    videoBounds.Markers.Add(
+                        new GMap.NET.WindowsForms.Markers.GMarkerGoogle(
+                            new GMap.NET.PointLatLng(point.Lat, point.Lng),
+                            GMap.NET.WindowsForms.Markers.GMarkerGoogleType.blue_small
+                        )
+                    );
+                }
+            }
+        }
+
+        private void VideoBox_MouseLeave(object sender, EventArgs e)
+        {
+            videoBounds.Markers.Clear();
         }
     }
 

shakebss/pilot

ExtLibs/ArduPilot/Mavlink/CameraProtocol.cs
@@ -1,9 +1,12 @@
-﻿using System;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Threading.Tasks;
 using Core.Geometry;
 using GeoAPI.DataStructures;
 using log4net;
+using MissionPlanner.Utilities;
 
 namespace MissionPlanner.ArduPilot.Mavlink
 {
@@ -22,9 +25,12 @@ namespace MissionPlanner.ArduPilot.Mavlink
         // Tracks whether we have received a `CAMERA_INFORMATION` message yet
         private bool have_camera_information = false;
 
-        public MAVLink.mavlink_camera_information_t CameraInformation { get; set; }
-        public MAVLink.mavlink_camera_settings_t CameraSettings { get; set; }
-        public MAVLink.mavlink_camera_capture_status_t CameraCaptureStatus { get; set; }
+        public MAVLink.mavlink_camera_information_t CameraInformation { get; private set; }
+        public MAVLink.mavlink_camera_settings_t CameraSettings { get; private set; }
+        public MAVLink.mavlink_camera_capture_status_t CameraCaptureStatus { get; private set; }
+        public MAVLink.mavlink_camera_fov_status_t CameraFOVStatus { get; private set; }
+
+        public static ConcurrentDictionary<(byte, byte, byte), MAVLink.mavlink_video_stream_information_t> VideoStreams { get; private set; } = new ConcurrentDictionary<(byte, byte, byte), MAVLink.mavlink_video_stream_information_t>();
 
         /// <summary>
         /// True if the camera has different modes, like image mode and video mode
@@ -161,13 +167,16 @@ namespace MissionPlanner.ArduPilot.Mavlink
             {
             case MAVLink.MAVLINK_MSG_ID.CAMERA_INFORMATION:
                 have_camera_information = true;
-                CameraInformation = ((MAVLink.mavlink_camera_information_t)message.data);
+                CameraInformation = (MAVLink.mavlink_camera_information_t)message.data;
                 break;
             case MAVLink.MAVLINK_MSG_ID.CAMERA_SETTINGS:
-                CameraSettings = ((MAVLink.mavlink_camera_settings_t)message.data);
+                CameraSettings = (MAVLink.mavlink_camera_settings_t)message.data;
                 break;
             case MAVLink.MAVLINK_MSG_ID.CAMERA_CAPTURE_STATUS:
-                CameraCaptureStatus = ((MAVLink.mavlink_camera_capture_status_t)message.data);
+                CameraCaptureStatus = (MAVLink.mavlink_camera_capture_status_t)message.data;
+                break;
+            case MAVLink.MAVLINK_MSG_ID.CAMERA_FOV_STATUS:
+                CameraFOVStatus = (MAVLink.mavlink_camera_fov_status_t)message.data;
                 break;
             }
         }
@@ -191,6 +200,19 @@ namespace MissionPlanner.ArduPilot.Mavlink
                 Task.Run(RequestCameraInformationAsync);
             }
 
+            // Request FOV status
+            Task.Run(async () =>
+            {
+                await parent.parent.doCommandAsync(
+                    parent.sysid, parent.compid,
+                    MAVLink.MAV_CMD.SET_MESSAGE_INTERVAL,
+                    (float)MAVLink.MAVLINK_MSG_ID.CAMERA_FOV_STATUS,
+                    interval_us,
+                    0, 0, 0, 0, 0
+                ).ConfigureAwait(false);
+            });
+
+            // Get camera settings
             if (HasModes || HasZoom || HasFocus)
             {
                 Task.Run(async () =>
@@ -323,6 +345,38 @@ namespace MissionPlanner.ArduPilot.Mavlink
                 zoom_level,
                 0, 0, 0, 0, 0
             );
+        }
+
+        public PointLatLngAlt CalculateImagePointLocation(double x, double y)
+        {
+            var imagePosition = new PointLatLngAlt(CameraFOVStatus.lat_image * 1e-7, CameraFOVStatus.lon_image * 1e-7, CameraFOVStatus.alt_image * 1e-3);
+            if(x == 0 && y == 0)
+            {
+                return imagePosition;
+            }
+
+            var camPosition = new PointLatLngAlt(CameraFOVStatus.lat_camera * 1e-7, CameraFOVStatus.lon_camera * 1e-7, CameraFOVStatus.alt_camera * 1e-3);
+
+            var height = camPosition.Alt - imagePosition.Alt;
+            if(height < 0)
+            {
+                return null;
+            }
+
+            var dist = camPosition.GetDistance(imagePosition);
+            var down_elevation = Math.Atan2(height, dist); // zero means pointing level, pi/2 is straight down
+            down_elevation += y / 2 * CameraFOVStatus.vfov * Math.PI / 180;
+            down_elevation = Math.Max(0.0001, down_elevation);
+            var out_distance = height * Math.Cos(down_elevation) / Math.Sin(down_elevation);
+            out_distance = Math.Min(out_distance, 1e5);
+
+            var side_angle = x / 2 * CameraFOVStatus.hfov * Math.PI / 180;
+            var side_distance = Math.Sqrt(out_distance * out_distance + height * height) * Math.Tan(side_angle);
+
+            var bearing = camPosition.GetBearing(imagePosition);
+            var pos = camPosition.newpos(bearing, out_distance).newpos(bearing + 90, side_distance);
+            pos.Alt = imagePosition.Alt;
+            return pos;
         }
     }
 }

GCSViews/GimbalVideoControl.cs
@@ -15,6 +15,7 @@ using static MAVLink;
 using MissionPlanner.GCSViews;
 using System.Threading.Tasks;
 using MissionPlanner.ArduPilot.Mavlink;
+using GMap.NET.WindowsForms;
 
 namespace MissionPlanner
 {
@@ -71,6 +72,7 @@ namespace MissionPlanner
             }
         }
 
+        private GMapOverlay mouseMapMarker;
         public GimbalVideoControl()
         {
             InitializeComponent();
@@ -81,6 +83,9 @@ namespace MissionPlanner
 
             // Register the global key handler
             Application.AddMessageFilter(this);
+
+            mouseMapMarker = new GMapOverlay("MouseMarker");
+            MainV2.instance.FlightData.gMapControl1.Overlays.Add(mouseMapMarker);
         }
 
         private void loadPreferences()
@@ -376,6 +381,62 @@ namespace MissionPlanner
                 var loc = MainV2.comPort?.MAV?.cs.HomeLocation;
                 selectedGimbalManager?.SetROILocationAsync(loc.Lat, loc.Lng, loc.Alt, frame: MAV_FRAME.GLOBAL);
             }
+        }
+
+        private DateTime lastMouseMove = DateTime.MinValue;
+        private void VideoBox_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if(DateTime.Now > lastMouseMove.AddMilliseconds(100))
+            {
+                lastMouseMove = DateTime.Now;
+                mouseMapMarker.Clear();
+
+                if (VideoBox.Image == null)
+                {
+                    return;
+                }
+
+                // Find the point within the image inside VideoBox, not just the VideoBox
+                var x = e.X;
+                var y = e.Y;
+                var imageWidth = Math.Min(VideoBox.Width, VideoBox.Height * VideoBox.Image.Width / VideoBox.Image.Height);
+                var imageHeight = Math.Min(VideoBox.Height, VideoBox.Width * VideoBox.Image.Height / VideoBox.Image.Width);
+                if (imageWidth < VideoBox.Width)
+                {
+                    x -= (VideoBox.Width - imageWidth) / 2;
+                    x *= VideoBox.Width / imageWidth;
+                    if (x < 0 || x >= imageWidth)
+                    {
+                        return;
+                    }
+                }
+                if (imageHeight < VideoBox.Height)
+                {
+                    y -= (VideoBox.Height - imageHeight) / 2;
+                    y *= VideoBox.Height / imageHeight;
+                    if (y < 0 || y >= imageHeight)
+                    {
+                        return;
+                    }
+                }
+
+
+                var point = selectedCamera?.CalculateImagePointLocation(2 * x / (float)imageWidth - 1, 2 * y / (float)imageHeight - 1);
+                if (point != null)
+                {
+                    mouseMapMarker.Markers.Add(
+                        new GMap.NET.WindowsForms.Markers.GMarkerGoogle(
+                            new GMap.NET.PointLatLng(point.Lat, point.Lng),
+                            GMap.NET.WindowsForms.Markers.GMarkerGoogleType.blue_small
+                        )
+                    );
+                }
+            }
+        }
+
+        private void VideoBox_MouseLeave(object sender, EventArgs e)
+        {
+            mouseMapMarker.Markers.Clear();
         }
     }
 